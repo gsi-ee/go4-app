88070e895565c0ab470336bc37c1fa312e26db14
augustin <augustin@302e7798-3865-0410-85c9-d38765bbeff6>
2015-11-25 11:20:22 +0000

    added a first example on how to use the wrapper around the PyROOT-bound go4-object and the pythonistic library
    git-svn-id: https://subversion.gsi.de/go4/app@1809 302e7798-3865-0410-85c9-d38765bbeff6

---
+++ go4py/example.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+print "Entering Python script"
+
+print
+print "before wrapper import:"
+print " go4 ==", go4
+print " no NextMatchingObject gives:", go4.NextMatchingObject("does_not_exist", "*", True)
+
+
+from go4py import go4
+#import go4py.go4 as go4
+
+
+print
+print "after wrapper import:"
+print " go4 ==", go4
+print " go4.analysis ==", go4.analysis
+print " no NextMatchingObject gives:", go4.NextMatchingObject("does_not_exist", "*", True)
+
+
+
+import go4py
+
+for h in go4py.MatchingObjects("*3", "Histograms"):
+    print h.GetName(), h.GetEntries()
+
+
+
+# reset go4 object to the PyROOT-bound go4
+go4 = go4.analysis
+print "Leaving Python script"
+
