a84baeb59839abcfbc681c4926190db9bb31d55d
augustin <augustin@302e7798-3865-0410-85c9-d38765bbeff6>
2016-04-05 20:51:25 +0000

    go4py examples: example for the converters
    git-svn-id: https://subversion.gsi.de/go4/app@1883 302e7798-3865-0410-85c9-d38765bbeff6

---
+++ go4py/converters.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import go4py
+
+
+f = go4py.ExpMem("export.root")
+
+ho = next(f.MatchingObjects("*g", "Histograms"))
+xo, yo = go4py.convert.hist2list(ho)
+
+hn = go4py.convert.list2hist(xo, yo)
+xn, yn = go4py.convert.hist2list(hn)
+
+
+def overview(l):
+    return "\t".join(str(i) for i in (type(l), len(l), min(l), max(l), sum(l)))
+
+print hn
+print "\t".join(i for i in ("type:\t", "len:", "min:", "max:", "sum:"))
+print overview(xo)
+print overview(xn)
+print overview(yo)
+print overview(yn)
+
+assert xo == xn
+assert yo == yn
+
+
+
