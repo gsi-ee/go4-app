e2a5f9dced27b361e4e4318b8298509a9833c18a
adamczew <adamczew@302e7798-3865-0410-85c9-d38765bbeff6>
2019-08-28 07:55:34 +0000

    hitdet analysis: add setup macro to change initial parameters without compiling
    git-svn-id: https://subversion.gsi.de/go4/app@2615 302e7798-3865-0410-85c9-d38765bbeff6

---
+++ hitdet/set_HitDetRawParam.C
@@ -0,0 +1,42 @@
+#define HitDet_FILTERCOEFFS 5
+#define HitDet_MAXBOARDS 10
+
+void set_HitDetRawParam()
+{
+#ifndef __GO4ANAMACRO__
+   std::cout << "Macro set_HitDetRawParam can execute only in analysis" << std::endl;
+   return;
+#endif
+   THitDetRawParam* param0 = (THitDetRawParam*) go4->GetParameter("HitDetRawParam","THitDetRawParam");
+
+   if (param0==0) {
+      TGo4Log::Error("Could not find parameter HitDetRawParam of class THitDetRawParam");
+      return;
+   }
+
+   TGo4Log::Info("Set parameter HitDetRawParam as provided on 28-aug-2019 by JAM");
+
+// uncomment lines below to override comiled/stored defaults:
+
+
+   for (int i = 0; i < HitDet_MAXBOARDS; ++i)
+     {
+     param0->fBoardID[i] = -1;
+     }
+
+   param0->fBoardID[0] = 42;    // this might be a unique hardware id
+
+   param0->fNumSnapshots = 64;
+   param0->fTraceLength = 16;
+   param0->fDoFFT=kTRUE;
+   param0-> fDoCalibrate=kFALSE;
+   param0->fFFTOptions="R2C M";
+   param0->fFilterType=0;
+   for (int i = 0; i < HitDet_FILTERCOEFFS; ++i)
+       {
+       param0->fFilterCoeff[i]=0;
+       }
+   
+
+
+}
