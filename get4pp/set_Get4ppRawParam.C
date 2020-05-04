e99a01ee2c0f8cc30cfef91e06697a04b98c9ddb
adamczew <adamczew@302e7798-3865-0410-85c9-d38765bbeff6>
2020-05-04 08:22:57 +0000

    get4++ analysis: update setup script
    git-svn-id: https://subversion.gsi.de/go4/app@2789 302e7798-3865-0410-85c9-d38765bbeff6

--- get4pp/set_Get4ppRawParam.C
+++ get4pp/set_Get4ppRawParam.C
@@ -1,4 +1,4 @@
-// written by macro saveparam.C at Wed Aug 28 10:03:11 2019
+// written by macro saveparam.C at Mon May  4 10:15:39 2020
 void set_Get4ppRawParam()
 {
 #ifndef __GO4ANAMACRO__
@@ -12,7 +12,7 @@ void set_Get4ppRawParam()
       return;
    }
 
-   TGo4Log::Info("Set parameter Get4ppRawParam as saved at Wed Aug 28 10:03:11 2019");
+   TGo4Log::Info("Set parameter Get4ppRawParam as saved at Mon May  4 10:15:39 2020");
 
    param0->fBoardID[0] = 42;
    param0->fBoardID[1] = -1;
@@ -24,16 +24,7 @@ void set_Get4ppRawParam()
    param0->fBoardID[7] = -1;
    param0->fBoardID[8] = -1;
    param0->fBoardID[9] = -1;
-//   param0->fNumSnapshots = 64;
-//   param0->fTraceLength = 8;
-//   param0->fDoCalibrate = kFALSE;
-//   param0->fDoFFT = kTRUE;
-//   param0->fFFTOptions = "R2C M";
-//   param0->fFilterType = 0;
-//   param0->fFilterCoeff[0] = 0;
-//   param0->fFilterCoeff[1] = 0;
-//   param0->fFilterCoeff[2] = 0;
-//   param0->fFilterCoeff[3] = 0;
-//   param0->fFilterCoeff[4] = 0;
+   param0->fVerbosity = 0;
+   param0->fSlowMotion = kFALSE;
 
 }
